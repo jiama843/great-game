e06646b522dfd59a45bf22b6a4ad8d3fa08de7ac
BrandenSoropia <bsoropia.dev@protonmail.com>
2024-05-11 16:18:21 -0400

    Fix spawn point for mob. Changed pixel per unit of monster sprite from 100 -> 10.

--- Assets/Scripts/ScriptableObjects/ManaScriptableObject.cs
+++ Assets/Scripts/ScriptableObjects/ManaScriptableObject.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ManaResource", menuName = "ScriptableObjects/ManaScriptableObject", order = 1)]
 public class ManaScriptableObject : ScriptableObject
 {
+    [SerializeField] int initialMana = 10;
     public int maxMana = 10;
     public int manaAmount = 10;
 
     // Apparenlty to "save" values across scenes, we need this. Source:
     // https://discussions.unity.com/t/persistence-of-scriptableobjects-between-scenes/237648/3 
-    private void OnEnable() => hideFlags = HideFlags.DontUnloadUnusedAsset;
+    private void OnEnable()
+    {
+        hideFlags = HideFlags.DontUnloadUnusedAsset;
+
+        // Reset SO on each play. Source:
+        // https://discussions.unity.com/t/how-are-you-resetting-your-scriptable-objects-between-play-tests/227242/2
+        maxMana = initialMana;
+        manaAmount = initialMana;
+    }
+
+
+    public void DecreaseMana(int amount)
+    {
+        manaAmount -= amount;
+    }
 }
